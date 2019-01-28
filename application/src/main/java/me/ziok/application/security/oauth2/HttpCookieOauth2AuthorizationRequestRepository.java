c0828a6e190d72e53fbfd8993af505881fb2cec8
junk-sound <wom2277@naver.com>
2019-01-28 12:38:36 +0900

    delete all about security but still unauthorized

--- application/src/main/java/me/ziok/application/security/oauth2/HttpCookieOauth2AuthorizationRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package me.ziok.application.security.oauth2;
-
-import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
-
-public interface HttpCookieOauth2AuthorizationRequestRepository extends AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
-}
